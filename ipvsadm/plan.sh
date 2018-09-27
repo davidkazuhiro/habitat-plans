54c20e8669e4caca2a540990f70334ebebf4d32f
David Somers-Harris <david.somers-harris@rakuten.com>
2018-09-27 19:18:13 +0900

    remove unused attributes

--- ipvsadm/plan.sh
+++ ipvsadm/plan.sh
@@ -5,7 +5,6 @@ pkg_maintainer="David Somers-Harris <david@somers-harris.com>"
 pkg_license=("Apache-2.0")
 pkg_source="https://mirrors.edge.kernel.org/pub/linux/utils/kernel/${pkg_name}/${pkg_name}-${pkg_version}.tar.gz"
 pkg_shasum="297f5cd459c3eef81ed0ca32e53bf320ed6b132fe7ed6ea5e44aa6b1fbd2a7de"
-# pkg_deps=(core/glibc)
 pkg_build_deps=(
   core/gcc
   core/make
@@ -15,28 +14,6 @@ pkg_build_deps=(
   core/which
   davidkazuhiro/libnl
 )
-# pkg_lib_dirs=(lib)
-# pkg_include_dirs=(include)
-# pkg_bin_dirs=(bin)
-# pkg_pconfig_dirs=(lib/pconfig)
-# pkg_svc_run="haproxy -f $pkg_svc_config_path/haproxy.conf"
-# pkg_exports=(
-#   [host]=srv.address
-#   [port]=srv.port
-#   [ssl-port]=srv.ssl.port
-# )
-# pkg_exposes=(port ssl-port)
-# pkg_binds=(
-#   [database]="port host"
-# )
-# pkg_binds_optional=(
-#   [storage]="port host"
-# )
-# pkg_interpreters=(bin/bash)
-# pkg_svc_user="hab"
-# pkg_svc_group="$pkg_svc_user"
-# pkg_description="Some description."
-# pkg_upstream_url="http://example.com/project-name"
 pkg_description="Ipvsadm is  used  to set up, maintain or inspect the virtual server table in the Linux kernel."
 pkg_upstream_url="http://kb.linuxvirtualserver.org/wiki/Ipvsadm"
 

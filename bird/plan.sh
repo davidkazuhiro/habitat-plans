2fd2ca209db1fc0bd1a8a906195d360046f1fedd
David Somers-Harris <david@somers-harris.com>
2018-10-05 18:24:34 +0900

    Updated from 1.6.4 to 2.0.2
    Signed-off-by: David Somers-Harris <david@somers-harris.com>

--- bird/plan.sh
+++ bird/plan.sh
@@ -1,18 +1,21 @@
 pkg_name=bird
 pkg_origin=davidkazuhiro
-pkg_version="1.6.4"
+pkg_version="2.0.2"
 pkg_maintainer="David Somers-Harris <david@somers-harris.com>"
 pkg_license=("Apache-2.0")
-pkg_source="ftp://bird.network.cz/pub/${pkg_name}/${pkg_name}-${pkg_version}.tar.gz"
+pkg_source="https://github.com/BIRD/bird/archive/v${pkg_version}.tar.gz"
 pkg_filename="${pkg_name}-${pkg_version}.tar.gz"
-pkg_shasum="c26b8caae988dba81a9dbbee93502463d4326d1b749d728d62aa5529c605afc0"
+pkg_shasum="bd42d48fbcc2c0046d544f1183cd98193ff15b792d332ff45f386b0180b09335"
 pkg_deps=(core/glibc)
 pkg_build_deps=(
-  core/make
-  core/gcc
-  core/flex
+  core/autoconf
   core/bison
-  core/m4
+  core/flex
+  core/gcc
+  #core/m4
+  core/make
+  core/ncurses
+  core/readline
 )
 pkg_svc_run="bird -f -u hab -g hab -c $pkg_svc_config_path/bird.conf"
 pkg_bin_dirs=(sbin)
@@ -22,7 +25,7 @@ pkg_description="BIRD Internet Routing Daemon"
 pkg_upstream_url="https://bird.network.cz/"
 
 do_build() {
-  ./configure --prefix=$pkg_prefix \
-              --disable-client
+  autoreconf
+  ./configure --prefix=${pkg_prefix}
   make
 }
